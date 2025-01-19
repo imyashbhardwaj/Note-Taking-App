aedfd9aa0ef3fde52af758b77666e6221fef387a
Yash Bhardwaj <bhardwajy60@yahoo.in>
2025-01-19 22:20:51 +0530

    Update

--- client/src/Home.tsx
+++ client/src/Home.tsx
@@ -1,12 +1,12 @@
-import { SignInButton, SignedIn, SignedOut } from '@clerk/clerk-react';
+import { SignedIn, SignedOut } from '@clerk/clerk-react';
 import SignedInUI from './SignedInUI';
+import SignInComponent from './SignIn';
 function Home() {
   return (
     <div>
       <center>
         <SignedOut>
-          <h2>Please Sign In/Sign Up to start Collaborating on notes</h2>
-          <SignInButton />
+          <SignInComponent />
         </SignedOut>
         <SignedIn>
           <SignedInUI />
